9a4bbfc2ed3c9d67592710540ec51009a609351b
crazymanpj <pang456jian@163.com>
2016-08-31 15:20:47 +0800

    python test code
    python test code

---
+++ test/test_for_ini.py
@@ -0,0 +1,12 @@
+import ConfigParser
+
+def writeinivalue(configfile, section, key, value):
+	config = ConfigParser.ConfigParser()
+	config.read(configfile)
+	if not config.has_section(section):
+		temp = config.add_section(section)
+	config.set(section, key, value)
+	config.write(open(configfile, "r+"))
+
+
+writeinivalue("mailconfig.ini", "searchmail", "start","112233")
---
+++ test/test_for_mongodb.py
@@ -0,0 +1,2 @@
+import pymongo
+conn= pymongo.MongoClient('localhost', 27017)
